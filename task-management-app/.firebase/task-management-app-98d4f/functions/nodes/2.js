efcfec2d11bde86f34d4f54fe93758eb4b6e8fcd
fbnmlnss <fabimilanes@gmail.com>
2023-07-04 13:23:02 -0600

    Deploy al sitio

---
+++ task-management-app/.firebase/task-management-app-98d4f/functions/nodes/2.js
@@ -0,0 +1,7 @@
+
+
+export const index = 2;
+export const component = async () => (await import('../entries/pages/_page.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/2.05c9666c.js","_app/immutable/chunks/scheduler.cc9b4bb6.js","_app/immutable/chunks/index.2442d2de.js"];
+export const stylesheets = [];
+export const fonts = [];
