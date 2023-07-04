aa264b7960076d3d13be4932421c8ae864a0c09e
fbnmlnss <fabimilanes@gmail.com>
2023-07-04 16:16:08 -0600

    Cambio deployment

--- task-management-app/.firebase/task-management-app-3efab/functions/manifest.js
+++ task-management-app/.firebase/task-management-app-3efab/functions/manifest.js
@@ -4,7 +4,7 @@ export const manifest = {
 	assets: new Set(["favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.a4a12641.js","app":"_app/immutable/entry/app.2a35a24e.js","imports":["_app/immutable/entry/start.a4a12641.js","_app/immutable/chunks/scheduler.cc9b4bb6.js","_app/immutable/chunks/singletons.e3010a93.js","_app/immutable/entry/app.2a35a24e.js","_app/immutable/chunks/scheduler.cc9b4bb6.js","_app/immutable/chunks/index.2442d2de.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.e9690aaa.js","app":"_app/immutable/entry/app.dfad4a58.js","imports":["_app/immutable/entry/start.e9690aaa.js","_app/immutable/chunks/scheduler.cc9b4bb6.js","_app/immutable/chunks/singletons.b6d39ab2.js","_app/immutable/entry/app.dfad4a58.js","_app/immutable/chunks/scheduler.cc9b4bb6.js","_app/immutable/chunks/index.2442d2de.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			() => import('./nodes/0.js'),
 			() => import('./nodes/1.js'),
