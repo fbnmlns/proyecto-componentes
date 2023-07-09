18e043c1ef0e3a23e3b446a1a79ca2ff6e1a7d34
Unxpected155 <gabripb2004@gmail.com>
2023-07-09 17:39:44 -0600

    Login funcional

--- task-management-app/src/routes/login/+page.svelte
+++ task-management-app/src/routes/login/+page.svelte
@@ -1,1 +1,58 @@
-<h1>LOGIN</h1>
+<!-- src/routes/login/+page.svelte -->
+<script lang="ts">
+    
+    import { goto } from '$app/navigation';
+    import { signInWithEmailAndPassword } from 'firebase/auth';
+    import { auth } from '../../lib/firebase/firebase';
+   
+let email: string;
+	let password: string;
+
+	let success: boolean | undefined = undefined;
+
+	const login = () => {
+		signInWithEmailAndPassword(auth, email, password)
+			.then(() => {
+				goto('/register');
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				console.log(errorCode, errorMessage);
+
+				success = false;
+			});
+	};
+</script>
+
+<svelte:head>
+	<title>Login</title>
+</svelte:head>
+
+<h1 class="text-4xl font-bold">Login</h1>
+
+<form
+	class="flex flex-col gap-4 p-8 space-y-4 bg-white sm:w-10/12"
+	on:submit|preventDefault={login}
+>
+	<input
+		type="email"
+		placeholder="Email"
+		class="px-4 py-2 border border-gray-300 rounded-md"
+		required
+		bind:value={email}
+	/>
+	<input
+		type="password"
+		placeholder="Password"
+		class="px-4 py-2 border border-gray-300 rounded-md"
+		required
+		bind:value={password}
+	/>
+
+	<button type="submit" class="default-action">Login</button>
+
+	{#if !success && success !== undefined}
+		<div class="p-8 text-red-500 bg-red-100">There was an error logging in. Please try again.</div>
+	{/if}
+</form>
