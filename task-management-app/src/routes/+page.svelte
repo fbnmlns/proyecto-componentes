695b567b51f347eeb91f27605c6aba0468df854e
Larkorin <michael162003@gmail.com>
2023-07-08 19:28:42 -0600

    <3

--- task-management-app/src/routes/+page.svelte
+++ task-management-app/src/routes/+page.svelte
@@ -1,22 +1,3 @@
 <svelte:head>
-	<title>SvelteKit authentication with Firebase</title>
+	<title>FUNCIONA :D AMOR PARA EL QUE LO LEA</title>
 </svelte:head>
-
-<h1 class="text-4xl font-bold">SvelteKit authentication with Firebase</h1>
-
-<p>
-	Blog post: <a
-		href="https://www.okupter.com/blog/client-side-authentication-firebase-sveltekit"
-		class="font-bold hover:text-blue-700 hover:underline"
-		target="_blank">https://www.okupter.com/blog/client-side-authentication-firebase-sveltekit</a
-	>
-</p>
-
-<p>
-	GitHub repository: <a
-		href="https://github.com/JustinyAhin/okupter-repos/tree/main/apps/sveltekit-auth-firebase"
-		class="font-bold hover:text-blue-700 hover:underline"
-		target="_blank"
-		>https://github.com/JustinyAhin/okupter-repos/tree/main/apps/sveltekit-auth-firebase</a
-	>
-</p>
